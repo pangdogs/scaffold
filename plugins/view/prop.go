4b1144d1b660a4ff50b6808f2a66e80409210b3f
pangdogs <pangdogs@gmail.com>
2024-09-20 19:18:23 +0800

    修改消息读取方式

--- plugins/view/prop.go
+++ plugins/view/prop.go
@@ -22,6 +22,7 @@ package view
 import (
 	"errors"
 	"git.golaxy.org/framework/net/gap/variant"
+	"git.golaxy.org/framework/utils/binaryutil"
 	"reflect"
 )
 
@@ -73,7 +74,7 @@ func (p *PropT[T]) Marshal() ([]byte, int64, error) {
 
 	bs := make([]byte, v.Size())
 
-	if _, err := v.Read(bs); err != nil {
+	if _, err := binaryutil.ReadToBuff(bs, v); err != nil {
 		return nil, 0, err
 	}
 
