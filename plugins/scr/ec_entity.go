2408450b2cb29fd6bb63716829169484e392eb56
pangdogs <pangdogs@gmail.com>
2024-12-12 16:53:38 +0800

    更新Golang脚本化逻辑

--- plugins/scr/ec_entity.go
+++ plugins/scr/ec_entity.go
@@ -21,6 +21,7 @@ package scr
 
 import (
 	"fmt"
+	"git.golaxy.org/core/ec"
 	"git.golaxy.org/core/ec/pt"
 	"git.golaxy.org/core/utils/types"
 	"git.golaxy.org/framework"
@@ -30,18 +31,18 @@ import (
 	"strings"
 )
 
-// Component 脚本化组件
-type Component struct {
-	framework.ComponentBehavior
+// Entity 脚本化实体
+type Entity struct {
+	framework.EntityBehavior
 }
 
 // Callee 被调函数
-func (c *Component) Callee(method string) reflect.Value {
+func (c *Entity) Callee(method string) reflect.Value {
 	return reflect.ValueOf(c.bindMethod(method))
 }
 
 // Awake 生命周期Awake
-func (c *Component) Awake() {
+func (c *Entity) Awake() {
 	method, _ := c.bindMethod("Awake").(func())
 	if method != nil {
 		method()
@@ -49,7 +50,7 @@ func (c *Component) Awake() {
 }
 
 // Start 生命周期Start
-func (c *Component) Start() {
+func (c *Entity) Start() {
 	method, _ := c.bindMethod("Start").(func())
 	if method != nil {
 		method()
@@ -57,7 +58,7 @@ func (c *Component) Start() {
 }
 
 // Shut 生命周期Shut
-func (c *Component) Shut() {
+func (c *Entity) Shut() {
 	method, _ := c.bindMethod("Shut").(func())
 	if method != nil {
 		method()
@@ -65,20 +66,33 @@ func (c *Component) Shut() {
 }
 
 // Dispose 生命周期Dispose
-func (c *Component) Dispose() {
+func (c *Entity) Dispose() {
 	method, _ := c.bindMethod("Dispose").(func())
 	if method != nil {
 		method()
 	}
 }
 
-func (c *Component) bindMethod(method string) any {
-	scriptPkg, ok := c.GetBuiltin().Extra.Get("script_pkg")
+// EntityEnableUpdate 脚本化实体，支持Update
+type EntityEnableUpdate struct {
+	Entity
+}
+
+// Update 生命周期Update
+func (c *EntityEnableUpdate) Update() {
+	method, _ := c.bindMethod("Update").(func())
+	if method != nil {
+		method()
+	}
+}
+
+func (c *Entity) bindMethod(method string) any {
+	scriptPkg, ok := c.GetPT().Extra().Get("script_pkg")
 	if !ok {
 		return nil
 	}
 
-	scriptIdent, ok := c.GetBuiltin().Extra.Get("script_ident")
+	scriptIdent, ok := c.GetPT().Extra().Get("script_ident")
 	if !ok {
 		return nil
 	}
@@ -91,39 +105,26 @@ func (c *Component) bindMethod(method string) any {
 	return thisMethod
 }
 
-// ComponentEnableUpdate 脚本化组件，支持Update
-type ComponentEnableUpdate struct {
-	Component
-}
-
-// Update 生命周期Update
-func (c *ComponentEnableUpdate) Update() {
-	method, _ := c.bindMethod("Update").(func())
-	if method != nil {
-		method()
-	}
-}
-
-// ComponentEnableLateUpdate 脚本化组件，支持LateUpdate
-type ComponentEnableLateUpdate struct {
-	Component
+// EntityEnableLateUpdate 脚本化实体，支持LateUpdate
+type EntityEnableLateUpdate struct {
+	Entity
 }
 
 // LateUpdate 生命周期LateUpdate
-func (c *ComponentEnableLateUpdate) LateUpdate() {
+func (c *EntityEnableLateUpdate) LateUpdate() {
 	method, _ := c.bindMethod("LateUpdate").(func())
 	if method != nil {
 		method()
 	}
 }
 
-// ComponentEnableUpdateAndLateUpdate 脚本化组件，支持Update、LateUpdate
-type ComponentEnableUpdateAndLateUpdate struct {
-	Component
+// EntityEnableUpdateAndLateUpdate 脚本化实体，支持Update、LateUpdate
+type EntityEnableUpdateAndLateUpdate struct {
+	Entity
 }
 
 // Update 生命周期Update
-func (c *ComponentEnableUpdateAndLateUpdate) Update() {
+func (c *EntityEnableUpdateAndLateUpdate) Update() {
 	method, _ := c.bindMethod("Update").(func())
 	if method != nil {
 		method()
@@ -131,20 +132,20 @@ func (c *ComponentEnableUpdateAndLateUpdate) Update() {
 }
 
 // LateUpdate 生命周期LateUpdate
-func (c *ComponentEnableUpdateAndLateUpdate) LateUpdate() {
+func (c *EntityEnableUpdateAndLateUpdate) LateUpdate() {
 	method, _ := c.bindMethod("LateUpdate").(func())
 	if method != nil {
 		method()
 	}
 }
 
-// ComponentWith 创建脚本化组件原型属性，用于注册实体原型时自定义相关属性
-func ComponentWith(name, script string, nonRemovable bool, extra ...map[string]any) pt.ComponentAttribute {
-	return ComponentWithT[Component](name, script, nonRemovable, extra...)
+// EntityWith 创建脚本化实体原型属性，用于注册实体原型时自定义相关属性
+func EntityWith(prototype, script string, scope *ec.Scope, componentAwakeOnFirstTouch, componentUniqueID *bool, extra ...map[string]any) pt.EntityAttribute {
+	return EntityWithT[Entity](prototype, script, scope, componentAwakeOnFirstTouch, componentUniqueID, extra...)
 }
 
-// ComponentWithT 创建脚本化组件原型属性，用于注册实体原型时自定义相关属性
-func ComponentWithT[T any](name, script string, nonRemovable bool, extra ...map[string]any) pt.ComponentAttribute {
+// EntityWithT 创建脚本化实体原型属性，用于注册实体原型时自定义相关属性
+func EntityWithT[T any](prototype, script string, scope *ec.Scope, componentAwakeOnFirstTouch, componentUniqueID *bool, extra ...map[string]any) pt.EntityAttribute {
 	idx := strings.LastIndexByte(script, '.')
 	if idx < 0 {
 		panic(fmt.Errorf("incorrect script %q format", script))
@@ -160,5 +161,5 @@ func ComponentWithT[T any](name, script string, nonRemovable bool, extra ...map[
 	_extra["script_pkg"] = scriptPkg
 	_extra["script_ident"] = scriptIdent
 
-	return pt.ComponentWith(types.ZeroT[T](), name, nonRemovable, _extra)
+	return pt.EntityWith(prototype, types.ZeroT[T](), scope, componentAwakeOnFirstTouch, componentUniqueID, _extra)
 }
