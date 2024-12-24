1256a20756727820c9faf6ec76db3df4244eb375
pangdogs <pangdogs@gmail.com>
2024-12-24 16:17:33 +0800

    整理代码

--- addins/view/propdelcare.go
+++ addins/view/propdelcare.go
@@ -27,41 +27,66 @@ import (
 	"reflect"
 )
 
+// PropCreatorT 属性创建器
 type PropCreatorT[T IPropSync] struct {
 	ps T
 }
 
+// SyncTo 设置同步目标
 func (c PropCreatorT[T]) SyncTo(to ...string) PropCreatorT[T] {
 	c.ps.setSyncTo(to)
 	return c
 }
 
+// Atti 设置Atti
 func (c PropCreatorT[T]) Atti(atti any) PropCreatorT[T] {
 	c.ps.setAtti(atti)
 	return c
 }
 
+// Reference 引用属性
 func (c PropCreatorT[T]) Reference() T {
 	return c.ps
 }
 
 // DeclarePropT 定义属性
 func DeclarePropT[T IPropSync](entity ec.Entity, name string) PropCreatorT[T] {
-	if entity == nil {
-		panic(fmt.Errorf("%s: entity is nil", core.ErrArgs))
-	}
-
-	ps := reflect.New(reflect.TypeFor[T]().Elem()).Interface().(T)
-	ps.Reset()
-	ps.init(Using(runtime.Current(entity)), entity, name, reflect.ValueOf(ps.Managed()))
-
-	entity.(IPropTab).AddProp(name, ps)
-
-	return PropCreatorT[T]{ps: ps}
+	return PropCreatorT[T]{ps: declareProp(entity, name, reflect.TypeFor[T]()).(T)}
 }
 
 // ReferencePropT 引用属性
 func ReferencePropT[T IPropSync](entity ec.Entity, name string) T {
+	return referenceProp(entity, name).(T)
+}
+
+// PropCreator 属性创建器
+type PropCreator = PropCreatorT[IPropSync]
+
+// DeclareProp 定义属性
+func DeclareProp(entity ec.Entity, name string, propRT reflect.Type) PropCreator {
+	return PropCreator{ps: declareProp(entity, name, propRT)}
+}
+
+// ReferenceProp 引用属性
+func ReferenceProp(entity ec.Entity, name string) IPropSync {
+	return referenceProp(entity, name)
+}
+
+func declareProp(entity ec.Entity, name string, propRT reflect.Type) IPropSync {
+	if entity == nil {
+		panic(fmt.Errorf("%s: entity is nil", core.ErrArgs))
+	}
+
+	prop := reflect.New(propRT).Interface().(IPropSync)
+	prop.Reset()
+	prop.init(Using(runtime.Current(entity)), entity, name, reflect.ValueOf(prop.Managed()))
+
+	entity.(IPropTab).AddProp(name, prop)
+
+	return prop
+}
+
+func referenceProp(entity ec.Entity, name string) IPropSync {
 	if entity == nil {
 		panic(fmt.Errorf("%s: entity is nil", core.ErrArgs))
 	}
@@ -71,5 +96,5 @@ func ReferencePropT[T IPropSync](entity ec.Entity, name string) T {
 		panic(fmt.Errorf("prop %s not found", name))
 	}
 
-	return prop.(T)
+	return prop
 }
