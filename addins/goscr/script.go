bc896579b2ec9bb7cff34d74ba9283bf0a94e462
pangdogs <pangdogs@gmail.com>
2025-05-30 15:02:23 +0800

    修改代码

--- addins/goscr/script.go
+++ addins/goscr/script.go
@@ -165,7 +165,7 @@ func (s *_Script) autoHotFix() {
 					return
 				}
 
-				log.Infof(s.svcCtx, "auto hotfix solution %q detecting %q -> %q changes, preparing to reload in 10s", s.options.PkgRoot, e.Op, e.Name)
+				log.Infof(s.svcCtx, "auto hotfix solution %q detecting %q -> %q changes, preparing to reload in 3s", s.options.PkgRoot, e.Op, e.Name)
 
 				s.reloading.Add(1)
 
