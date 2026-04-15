c08f1784e3651f618e9e785b8070ac2f1de1fd31
pangdogs <pangdogs@gmail.com>
2026-04-15 10:11:02 +0800

    整理代码

--- tools/protoc-gen-gdscript-excel/excelutils/excel_utils.gd
+++ tools/protoc-gen-gdscript-excel/excelutils/excel_utils.gd
@@ -1,3 +1,20 @@
+# This file is part of Golaxy Distributed Service Development Framework.
+#
+# Golaxy Distributed Service Development Framework is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# Golaxy Distributed Service Development Framework is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Golaxy Distributed Service Development Framework. If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (c) 2024 pangdogs.
+#
 # Shared helpers used by generated Excel table access code.
 # The generated code relies on this runtime for index conversion,
 # dictionary key ordering, and a few small binary-search utilities.
